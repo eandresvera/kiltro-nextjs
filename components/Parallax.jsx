59b1db6a0bde6de432c2dc659b5ebc8fc9673aeb
Andres <eandres.vera@gmail.com>
2023-02-09 00:25:01 -0300

    parallaax effect in progress

--- components/Parallax.jsx
+++ components/Parallax.jsx
@@ -1,45 +1,59 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 export const Parallax = ({ parallaxImg }) => {
-
   const [scrollPos, setScrollPos] = useState(0);
 
 
+
     const imageRef = useRef(null);
-  
     useEffect(() => {
       const handleScroll = () => {
         const scrollTop = window.pageYOffset;
         setScrollPos(scrollTop);
       };
   
-      window.addEventListener('scroll', handleScroll, true);
-      return () => window.removeEventListener('scroll', handleScroll, true);
+      window.addEventListener('scroll', handleScroll);
+      return () => window.removeEventListener('scroll', handleScroll);
     }, []);
-  
+
     useEffect(() => {
       const imageTop = imageRef.current.offsetTop;
       const imageHeight = imageRef.current.offsetHeight;
       const windowHeight = window.innerHeight;
   
-      let parallax = (scrollPos - imageTop) / windowHeight * 300;
+      let parallax = (scrollPos - imageTop) / windowHeight * 200;
   
-      parallax = Math.min(parallax, 10);
-      parallax = Math.max(parallax, -10);
+      parallax = Math.min(parallax, 50);
+      parallax = Math.max(parallax, -50);
   
       imageRef.current.style.backgroundPositionY = `${parallax}px`;
     }, [scrollPos]);
+  
+    // useEffect(() => {
+    //   const handleScroll = () => {
+    //     const scrollTop = window.pageYOffset;
+    //     const imageTop = imageRef.current.offsetTop;
+    //     const imageHeight = imageRef.current.offsetHeight;
+    //     const windowHeight = window.innerHeight;
+  
+    //     let parallax = (scrollTop - imageTop) / windowHeight * 200;
+  
+    //     imageRef.current.style.backgroundPositionY = `${parallax}px`;
+    //   };
+  
+    //   window.addEventListener('scroll', handleScroll);
+    //   return () => window.removeEventListener('scroll', handleScroll);
+    // }, []);
+  
   
     return (
       <div
         ref={imageRef}
         style={{
           backgroundImage: 'url(https://admin.kiltrobcn.com/wp-content/uploads/2023/02/pisco_sour-2.jpeg)',
-          backgroundSize: 'cover',
           height: '300px',
           width: '100%',
-          backgroundAttachment: 'fixed',
-          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
         }}
       />
     );
