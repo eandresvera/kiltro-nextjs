b11482c6c8701f40cb7af7701904ee56eb494c30
Andres <eandres.vera@gmail.com>
2023-02-11 00:55:57 -0300

    Footer component created, MAinLayout component created

--- pages/cart.js
+++ pages/cart.js
@@ -1,7 +1,11 @@
+import { useAppContext } from '@/components/context/AppContext';
 import { MainLayout } from '@/components/layout/MainLayout';
 import React from 'react'
 
 const Cart = () => {
+
+  const { homeData } = useAppContext();
+  
   return (
     <MainLayout>cart</MainLayout>
   )
--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,10 @@
+import { useAppContext } from "@/components/context/AppContext";
 import { MainLayout } from "@/components/layout/MainLayout";
 
 const Contact = () => {
+
+    const { homeData } = useAppContext();
+
     return(
         <MainLayout>Contact page</MainLayout>
     )
