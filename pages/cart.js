85febe8d66580f65232c884d46a9fde2c7e1bf2c
Andres <eandres.vera@gmail.com>
2023-02-15 17:12:20 -0300

    Cart page

--- pages/cart.js
+++ pages/cart.js
@@ -8,17 +8,19 @@ import Image from 'next/image';
 const Cart = ({ data }) => {
 
   const { homeData, updateHomeData } = useAppContext();
-
+  
   useEffect(() => {
     updateHomeData(data);
-      
+    
   }, [updateHomeData, data])
   
   if (!homeData) {
     return null;
   }
+
+  const cartImgs = homeData.nodes[0].cartImgs.nodes;
   
-  console.log(homeData);
+  // console.log(cartImgs);
 
   return (
     <MainLayout>
@@ -27,16 +29,19 @@ const Cart = ({ data }) => {
           <Image className='md:hidden fade-in' src="https://admin.kiltrobcn.com/wp-content/uploads/2023/02/kiltro_comida.jpg" alt="" fill/>
         </div>
 
-        <div className='min-h-screen pt-10 flex flex-col  items-center justify-center pb-10 space-y-10'>
-          <div className=' w-[90vw] md:w-[800px] h-[1080px] md:h-[1080px] relative'>
-            <Image src="https://admin.kiltrobcn.com/wp-content/uploads/2023/02/carta_comida1.jpeg" fill alt=""/>
-            {/* <Image src="https://admin.kiltrobcn.com/wp-content/uploads/2023/02/carta_comida1.jpeg" width="800" height="800" alt=""/> */}
-          </div>
-          <div className=' w-[90vw] md:w-[800px] h-[1080px] md:h-[1080px] relative'>
-            {/* <Image src="https://admin.kiltrobcn.com/wp-content/uploads/2023/02/carta_comida1.jpeg" fill alt=""/> */}
-            <Image src="https://admin.kiltrobcn.com/wp-content/uploads/2023/02/carta_cocktails_29Junio.jpeg" fill alt=""/>
-            {/* <Image src="https://admin.kiltrobcn.com/wp-content/uploads/2023/02/carta_comida1.jpeg" width="800" height="800" alt=""/> */}
-          </div>
+        <div className='min-h-screen mx-10 pt-10 flex flex-col items-center justify-center pb-10 space-y-10 relative'>
+          {
+            cartImgs.map( img => (
+              
+              <Image 
+                key={img.sourceUrl} 
+                src={img.sourceUrl} 
+                width="800" 
+                height="800" 
+                alt="Carta kiltro 1"/>
+            ) )
+            
+          }
         </div>
       </div>
     </MainLayout>
