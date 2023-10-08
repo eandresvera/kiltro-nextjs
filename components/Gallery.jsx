7b706ed3fc1621f995a99b29f687d246bb553055
eandresvera <eandres.vera@gmail.com>
2023-10-07 22:50:26 -0300

    UPDATE: alt image

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -38,8 +38,6 @@ export const Gallery = () => {
                                             sizes={img.sizes}
                                         /> 
                                     </div>
-                                {/* <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" /> */}
                                 </div>
                             </div>
                         ))
--- pages/menu.js
+++ pages/menu.js
@@ -24,7 +24,13 @@ const Menu = ({ data }) => {
     <MainLayout>
       <div className="min-h-screen text-white">
         <div className="h-[300px] md:h-[500px] md:bg-[url('https://admin.kiltrobcn.com/wp-content/uploads/2023/02/kiltro_comida.jpg')] bg-cover bg-no-repeat relative fade-in" >
-          <Image className='md:hidden fade-in' priority src="https://admin.kiltrobcn.com/wp-content/uploads/2023/02/kiltro_comida.jpg" alt="" fill/>
+          <Image 
+            className='md:hidden fade-in' 
+            priority 
+            src="https://admin.kiltrobcn.com/wp-content/uploads/2023/02/kiltro_comida.jpg" 
+            alt="Carta Kiltro restobar" 
+            fill
+          />
         </div>
 
         <div className='min-h-screen mx-10 pt-10 flex flex-col items-center justify-center pb-10 space-y-10 relative'>
